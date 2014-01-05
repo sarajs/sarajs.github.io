636fe25c52651df4c2b78a7a01e9b309d9157bf8
Jackson Gariety <personal@jacksongariety.com>
2014-01-04 16:23:40 -0800

    Working sara website.

---
+++ index.js
@@ -0,0 +1,12 @@
+void function ritz(node) {
+  node = node || document.body;
+  if (node.nodeType == 3 && node.data.indexOf("&") != -1 && ["BODY", "SCRIPT", "STYLE"].indexOf(node.nodeName) == -1) {
+    node.parentNode.innerHTML = node.data.replace(/&/, "<span style=\"font-family: Baskerville; font-style: italic; font-weight: 100;\">&</span>");
+  }
+  
+  node = node.firstChild
+  while (node) {
+    ritz(node)
+    node = node.nextSibling
+  }
+}()
